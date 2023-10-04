632d79d4a59918d638aceda1f005bbdcef34095c
DevopsFuzzie <139812446+DevopsFuzzie@users.noreply.github.com>
2023-10-04 21:24:11 +0100

    #25 refactor s3 bucket into the new terrahouse_aws module (#26)

--- main.tf
+++ main.tf
@@ -1,12 +1,14 @@
 
-
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
-resource "aws_s3_bucket" "website_bucket" {
-  # Bucket naming rules
-  # https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html
-  bucket = var.bucket_name
-
- tags = {
-  UserUuid = var.user_uuid
-  }
+#   cloud {
+#    organization = "DevopsFuzzie"
+#
+#    workspaces {
+#      name = "Terra-house-1"
+#    }
+#  }
+  
+module "terrahouse_aws" {
+  source = "./modules/terrahouse_aws"
+  user_uuid = var.user_uuid
+  bucket_name = var.bucket_name
 }
